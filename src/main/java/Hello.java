24e615f8bed874dd4720b23b20c5a353d130ba88
serkanyildirimer <serkantitx@gmail.com>
2023-01-12 18:20:04 +0300

    add more hello work

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("local Repo is already published to Github");
+        System.out.println("doing more work");
 
 
     }
